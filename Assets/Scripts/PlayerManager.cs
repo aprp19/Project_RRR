9ac7c20489f7c98d838a14da68c49249bf97bd53
Angga Puja Restu Prakasa <kierska19@gmail.com>
2023-04-26 00:21:41 +0700

    Partial Completion

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public GameObject deathUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Death"))
+        {
+            Time.timeScale = 0;
+            deathUI.SetActive(true);
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D _rb;
+    BoxCollider2D _boxCollider2D;
     SpriteRenderer _spriteRenderer;
     Animator _anim;
+
+    [SerializeField] LayerMask _jumpableGround;
     
     float _dirX = 0f;
     [SerializeField]float _moveSpeed = 7f;
     [SerializeField]float _jumpForce = 7f;
+    
+    enum _MovementState { idle, running, jumping, falling}
+    
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _boxCollider2D = GetComponent<BoxCollider2D>();
         _anim = GetComponent<Animator>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
     }
@@ -25,7 +32,7 @@ public class PlayerMovement : MonoBehaviour
         _dirX = Input.GetAxisRaw("Horizontal");
         _rb.velocity = new Vector2(_dirX * _moveSpeed, _rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && _isGrounded())
         {
             _rb.velocity = new Vector2(_rb.velocity.x, _jumpForce);
         }
@@ -34,20 +41,37 @@ public class PlayerMovement : MonoBehaviour
 
     private void UpdateAnimationState()
     {
+        _MovementState state;
         if (_dirX > 0f)
         {
-            _anim.SetBool("isRunning", true);
+            state = _MovementState.running;
             _spriteRenderer.flipX = false;
 
         }
         else if (_dirX < 0f)
         {
-            _anim.SetBool("isRunning", true);
+            state = _MovementState.running;
             _spriteRenderer.flipX = true;
         }
         else
         {
-            _anim.SetBool("isRunning", false);
+            state = _MovementState.idle;
         }
+
+        if (_rb.velocity.y > .1f)
+        {
+            state = _MovementState.jumping;
+        }
+        else if (_rb.velocity.y < -.1f)
+        {
+            state = _MovementState.falling;
+        }
+        
+        _anim.SetInteger("state", (int)state);
+    }
+
+    bool _isGrounded()
+    {
+        return Physics2D.BoxCast(_boxCollider2D.bounds.center, _boxCollider2D.bounds.size, 0f, Vector2.down, .1f, _jumpableGround);
     }
 }
