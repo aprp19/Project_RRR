269ace1fa71d9c5f170cdbf2e04b88218284be4f
Angga Puja Restu Prakasa <kierska19@gmail.com>
2023-04-26 22:49:03 +0700

    Adding parallax background
    Took 32 minutes

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,18 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public void RestartGame()
+    { 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ExitGame()
     {
-        
+        SceneManager.LoadScene("MainMenu");
     }
 }
