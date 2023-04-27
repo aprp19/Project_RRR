aecb7fb98d281fc49b4cc42facfe5cb96f517419
Angga Puja Restu Prakasa <kierska19@gmail.com>
2023-04-27 14:39:00 +0700

    Scene 1 Complete
    Adding 2D URP, Typewriter Effects, Character Chatbox
    
    Took 1 hour 17 minutes

---
+++ Assets/Scripts/ChatboxManager.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChatboxManager : MonoBehaviour
+{
+    public GameObject chat1, chat2, chat3;
+}
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,16 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerManager : MonoBehaviour
 {
     public GameObject deathUI;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject c1, c2, c3, c4;
 
     void OnCollisionEnter2D(Collision2D col)
     {
@@ -18,6 +16,44 @@ public class PlayerManager : MonoBehaviour
         {
             Time.timeScale = 0;
             deathUI.SetActive(true);
+        }
+
+        if (col.gameObject.CompareTag("Chatbox1"))
+        {
+            GameObject.FindWithTag("Chatbox1").SetActive(false);
+            c1.SetActive(true);
+        }
+        
+        if (col.gameObject.CompareTag("Chatbox2"))
+        {
+            GameObject.FindWithTag("Chatbox2").SetActive(false);
+            c1.SetActive(false);
+            c2.SetActive(true);
+        }
+        
+        if (col.gameObject.CompareTag("Chatbox3"))
+        {
+            GameObject.FindWithTag("Chatbox3").SetActive(false);
+            c2.SetActive(false);
+            c3.SetActive(true);
+        }
+
+        if (col.gameObject.CompareTag("Chatbox4"))
+        {
+            GameObject.FindWithTag("Chatbox4").SetActive(false);
+            c4.SetActive(true);
+        }
+
+        if (col.gameObject.CompareTag("CloseChat"))
+        {
+            GameObject.FindWithTag("CloseChat").SetActive(false);
+            c3.SetActive(false);
+            c4.SetActive(false);
+        }
+
+        if (col.gameObject.CompareTag("NextScene"))
+        {
+            SceneManager.LoadScene("Gameplay - Stage 2");
         }
     }
 }
